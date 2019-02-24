3e722b71b1b706fbc4e5bd7b75eda801e9664230
yairar <yair.ar@gmail.com>
2019-02-24 21:13:28 +0200

    support for GIF in InfoWindow

--- js/map.js
+++ js/map.js
@@ -4,33 +4,37 @@
 var infowindow;
 
 var locations = [
-["עין אבו כלאב", "31.67472884881077", "35.11300869279057", "3", "einabukleb", 	"point"],
-["נחל הפירים", "31.673444933876777", "35.16139409634275", "7", "nahalpirim", 	"point"],
-["חירבת ג'ומג'ום", "31.67120691075308", "35.101196243917116", "5", "hirbetjumjum", "point"],
-["בת עין", "31.655733364181664", "35.11108121964128", "3", "batain", 			"point"],
-["עין יצחק-סג'מה", "31.659591495439255", "35.11299796358264", "6", "einsajme", 	"point"],
-["עין ליבנה", "31.66331083992055", "35.09675554129274", "4", "einlivne", 		"point"],
-["מנזר הרוסי", "31.64585516703987", "35.124207950560276", "6", "minzarrusi", 	"point"],
-["כרמים", "31.659979910276387", "35.12301168533952", "1", "kramim", 			"point"],
-["נצר", "31.658796991800315", "35.14056034341968", "2", "netzer", 				"point"],
-["עין חובלה", "31.659049072956652", "35.10770700070066", "1", "einhubela", 	    "point"],
-["גבעת התמר", "31.676973284252718", "35.16549173755652", "2", "givathatamar", 	"point"],
-["סובב דגן", "31.682072554314008", "35.176170385805676", "5", "sovevdagan", 	"point"],
-["מצפור האלף", "31.683083711474527", "35.14508791260869", "2", "mitzporhaelef", "point"],
-["נווה דניאל", "31.67790230573368", "35.142750061480115", "8", "nevedaniel", 	"point"],
-["תל עיטם", "31.682836391980853", "35.17830438904912", "2", "teleitam", 		"point"],
-["עין צורים", "31.66833548860177", "35.12196562382371", "2", "eintzurim", 		"point"],
-["עוז וגאון", "31.641550053820243", "35.13378880116147", "5", "ozvegaon", 		"point"],
-["גבעת עוז", "31.637193053100415", "35.13731322380704", "1", "givatoz", 		"point"],
-["שדה בועז", "31.69270441115536", "35.14691553208036", "4", "sdeboaz", 		    "point"],
-["נחל צופית", "31.655759794152385", "35.103249169317905", "1", "nahaltsofit", 	"point"],
-["גבעת הזית", "31.674278786670797", "35.16704282852811", "1", "givathazait", 	"point"],
-["אמת הביאר", "31.656467565110308", "35.14638981911344", "2", "amathabiyar", 	"point"],
-["אפרת - רימון", "31.6501476645803", "35.14941535088224", "1", "efratrimon", 	"point"],
-["עין דובק", "31.65990588537998", "35.113264537779514", "1", "eindubek", 		"point"],
-["נחל תקוע", "31.64042592339606", "35.23539010448462", "8", "nahaltkoa",        "point"],
-['גבעת החי"ש', "31.65734981428064", "35.134184993934696", "3", "givathahish",    "point"],
+// [0] - Title in Hebrew, [1,2] - Position, [3]-Number of photos, [4]-Base name for all photos, [5]-Type of marker(point/...)
+
+/*0*/["עין אבו כלאב", "31.67472884881077", "35.11300869279057", "3", "einabukleb", 	    "point", "jpg"],
+["נחל הפירים", "31.673444933876777", "35.16139409634275",       "7", "nahalpirim", 	    "point", "jpg"],
+["חירבת ג'ומג'ום", "31.67120691075308", "35.101196243917116",   "5", "hirbetjumjum",    "point", "jpg"],
+["בת עין", "31.655733364181664", "35.11108121964128",           "3", "batain", 			"point", "jpg"],
+["עין יצחק-סג'מה", "31.659591495439255", "35.11299796358264",   "6", "einsajme", 	    "point", "jpg"],
+["עין ליבנה", "31.66331083992055", "35.09675554129274",         "4", "einlivne", 		"point", "jpg"],
+["מנזר הרוסי", "31.64585516703987", "35.124207950560276",       "6", "minzarrusi", 	    "point", "jpg"],
+["כרמים", "31.659979910276387", "35.12301168533952",            "1", "kramim", 			"point", "jpg"],
+["נצר", "31.658796991800315", "35.14056034341968",              "2", "netzer", 			"point", "jpg"],
+["עין חובלה", "31.659049072956652", "35.10770700070066",        "1", "einhubela", 	    "point", "jpg"],
+/*10*/["גבעת התמר", "31.676973284252718", "35.16549173755652",  "2", "givathatamar", 	"point", "jpg"],
+["סובב דגן", "31.682072554314008", "35.176170385805676",        "5", "sovevdagan", 	    "point", "jpg"],
+["מצפור האלף", "31.683083711474527", "35.14508791260869",       "2", "mitzporhaelef",   "point", "jpg"],
+["נווה דניאל", "31.67790230573368", "35.142750061480115",       "8", "nevedaniel", 	    "point", "jpg"],
+["תל עיטם", "31.682836391980853", "35.17830438904912",          "2", "teleitam", 		"point", "jpg"],
+["עין צורים", "31.66833548860177", "35.12196562382371",         "2", "eintzurim", 		"point", "jpg"],
+["עוז וגאון", "31.641550053820243", "35.13378880116147",        "5", "ozvegaon", 		"point", "jpg"],
+["גבעת עוז", "31.637193053100415", "35.13731322380704",         "1", "givatoz", 		"point", "jpg"],
+["שדה בועז", "31.69270441115536", "35.14691553208036",          "4", "sdeboaz", 	    "point", "jpg"],
+["נחל צופית", "31.655759794152385", "35.103249169317905",       "1", "nahaltsofit", 	"point", "jpg"],
+/*20*/["גבעת הזית", "31.674278786670797", "35.16704282852811",  "1", "givathazait", 	"point", "jpg"],
+["אמת הביאר", "31.656467565110308", "35.14638981911344",        "2", "amathabiyar", 	"point", "jpg"],
+["אפרת - רימון", "31.6501476645803", "35.14941535088224",       "1", "efratrimon", 	    "point", "jpg"],
+["עין דובק", "31.65990588537998", "35.113264537779514",         "1", "eindubek", 		"point", "jpg"],
+["נחל תקוע", "31.64042592339606", "35.23539010448462",          "8", "nahaltkoa",       "point", "jpg"],
+['גבעת החי"ש', "31.65734981428064", "35.134184993934696",       "3", "givathahish",     "point", "jpg"],
+['הרודיון', "31.66572385413287", "35.24166647357947",           "4", "herodion",        "point", "gif"]
 ];
+
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -77,10 +81,11 @@ function setMarkers(map) {
                 map: map,
                 icon: image,
                 shape: shape,
-                title: point[4],
+                title: point[4],    // Base name of file for all photos
                 zIndex: parseInt(point[3]),
-                label: point[0],
-                animation: google.maps.Animation.DROP
+                label: point[0],    // title in Hebrew
+                animation: google.maps.Animation.DROP,
+                
             });
             marker.addListener('click', showInfoWindow);
         });
@@ -94,6 +99,9 @@ function showInfoWindow() {
     }
     
     var name = this.getTitle();
+    var locationsEntryIndex = findEntryByName(name);
+    var infoWindowImageExtension = locations[locationsEntryIndex][6];
+    
     var label = this.getLabel();
     var numOfPhotos = this.getZIndex();
     var photos = [];
@@ -101,7 +109,7 @@ function showInfoWindow() {
         photos.push({href: 'images/map/big/'+name+i+'.jpg', title: label});
     }
     /*console.log(photos);*/
-    var contentHtml = '<p align="center">'+label+'<br><a href="#"><img src="images/map/'+name+'.jpg" id="'+name+'"></a></p>';
+    var contentHtml = '<p align="center">'+label+'<br><a href="#"><img src="images/map/'+name+'.'+infoWindowImageExtension+'" id="'+name+'"></a></p>';
 
     infowindow = new google.maps.InfoWindow({
             content: contentHtml,
@@ -123,3 +131,14 @@ function showInfoWindow() {
 
 } /* showInfoWindow */
 
+
+/* returns index of entry in locations array */
+function findEntryByName(name) {
+    
+    for (var i=0; i<locations.length; i++)
+    {
+        if (name == locations[i][4]) {
+            return i;
+        }
+    }
+}
