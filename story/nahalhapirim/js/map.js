c26417dd6a5ace0f075af2fee79026567af7139c
yairar <yairar@users.noreply.github.com>
2021-10-20 00:20:40 +0300

    new story/nahalhapirim

---
+++ story/nahalhapirim/js/map.js
@@ -0,0 +1,80 @@
+// The following example creates complex markers retrieved from array
+
+// Globals
+var infowindow;
+
+var locations = [
+// [0] - Title in Hebrew, [1,2] - Position, [3]-Number of photos, [4]-Base name for all photos, [5]-Type of marker(point/...)
+
+["נחל הפירים", "31.674398300128082", "35.161425508689945",      "79", "nahalpirim", 	"point", "jpg"],
+['עין אל מערה',"31.67050868087125", "35.15433374805457",        "10",  "einelmeara",     "point", "jpg"],
+['אמת הביאר',  "31.66040868087125", "35.15003374805457",       	"10",  "amathabiar",	    "point", "jpg"],
+];
+
+
+function initMap() {
+	
+	var mymap = L.map('map').setView([31.67050868087125, 35.15433374805457], 15);
+	
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(mymap);
+	
+	setMarkers(mymap);
+}
+
+function setMarkers(map) {
+    // Adds markers to the map.
+    
+	var img_url='';
+	var label, title_txt, popupHtml, numOfPhotos;
+	
+    /* Reading locations from array */
+	locations.forEach(function(point) {
+		/*
+			label: point[0], // title in Hebrew
+			Latitude: point[1],
+			Longtitude: point[2],
+			Number of photos: point[3],
+			title: point[4], // Base name of file for all photos
+		*/
+		label = point[0];
+		numOfPhotos = point[3];
+		title_txt = point[4];		
+		if (point[5] != null) {
+			img_url = 'images/map/'+title_txt+'.jpg';
+		}
+		popupHtml = '<img src="'+img_url+'" width="100%"><br><div align="center">'+label+'</div>';
+		var photos = [];
+		for (var i=1; i<=numOfPhotos; i++) {
+			photos.push({href: 'images/map/big/'+title_txt+i+'.jpg', title: '('+i+' of '+numOfPhotos+') '+label});
+		}
+		
+		var min_width = 300;
+		if (window.screen.width < 1000) {
+			min_width = 100;
+		}
+		var popup = L.popup({minWidth: min_width})
+		             .setContent(popupHtml);
+					 
+		L.marker([parseFloat(point[1]), parseFloat(point[2])], {title: label})
+		 .bindTooltip(label)
+		 .addTo(map)
+		 .bindPopup(popup)
+		 .on('click', function( e ) {
+						//e.preventDefault();
+						jQuery(function($) {
+							$.swipebox(photos);
+						});
+					  }
+         );  
+    });
+
+} /* setMarkers */
+
+initMap();
---
+++ story/nahalhapirim/js/photos.js
@@ -0,0 +1,97 @@
+var items = [
+{src:'images/map/big/nahalpirim57.jpg',w:1200, h:675, title:'נחל הפירים', tags: 'nahalpirim winter snow'},		
+{src:'images/map/big/nahalpirim28.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'nahalpirim winter'},
+{src:'images/map/big/nahalpirim46.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'nahalpirim winter'},
+{src:'images/map/big/nahalpirim24.jpg',w:1200, h:801, title:'נחל הפירים', tags: 'nahalpirim winter sunset'},
+{src:'images/map/big/nahalpirim48.jpg',w:1200, h:800, title:'נחל הפירים', tags: 'nahalpirim winter'},
+{src:'images/map/big/nahalpirim71.jpg',w:1400, h:578, title:'נחל הפירים', tags: 'nahalpirim winter'},
+{src:'images/map/big/nahalpirim72.jpg',w:1200, h:676, title:'נחל הפירים', tags: 'nahalpirim winter'},
+{src:'images/map/big/nahalpirim78.jpg',w:1400, h:778, title:'נחל הפירים', tags: 'nahalpirim winter'},
+{src:'images/map/big/nahalpirim79.jpg',w:1200, h:800, title:'נחל הפירים', tags: 'nahalpirim winter'},
+
+{src:'images/map/big/nahalpirim8.jpg', w:1200, h:800, title:'נחל הפירים', tags: 'nahalpirim grapes autumn'},
+{src:'images/map/big/nahalpirim2.jpg', w:1200, h:801, title:'נחל הפירים', tags: 'nahalpirim grapes spring'},
+{src:'images/map/big/nahalpirim4.jpg', w:1200, h:675, title:'נחל הפירים', tags: 'nahalpirim grapes autumn'},
+{src:'images/map/big/nahalpirim15.jpg',w:1200, h:800, title:'נחל הפירים', tags: 'nahalpirim grapes autumn'},
+{src:'images/map/big/nahalpirim33.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'nahalpirim grapes autumn'},
+{src:'images/map/big/nahalpirim34.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'nahalpirim grapes autumn'},
+{src:'images/map/big/nahalpirim31.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'nahalpirim grapes autumn'},
+{src:'images/map/big/nahalpirim42.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'nahalpirim grapes summer'},
+{src:'images/map/big/nahalpirim43.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'nahalpirim grapes summer'},
+{src:'images/map/big/nahalpirim44.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'nahalpirim grapes summer'},
+{src:'images/map/big/nahalpirim47.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'nahalpirim grapes autumn'},
+{src:'images/map/big/nahalpirim48.jpg',w:1200, h:800, title:'נחל הפירים', tags: 'nahalpirim grapes winter'},
+{src:'images/map/big/nahalpirim50.jpg',w:1200, h:800, title:'נחל הפירים', tags: 'nahalpirim grapes summer'},
+{src:'images/map/big/nahalpirim51.jpg',w:1200, h:800, title:'נחל הפירים', tags: 'nahalpirim grapes summer'},
+{src:'images/map/big/nahalpirim60.jpg',w:1200, h:801, title:'נחל הפירים', tags: 'nahalpirim grapes winter'},
+{src:'images/map/big/nahalpirim61.jpg',w:1200, h:868, title:'נחל הפירים', tags: 'nahalpirim grapes winter'},
+{src:'images/map/big/nahalpirim62.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'nahalpirim grapes autumn'},
+{src:'images/map/big/nahalpirim63.jpg',w:1200, h:800, title:'נחל הפירים', tags: 'nahalpirim grapes autumn'},
+{src:'images/map/big/nahalpirim64.jpg',w:1200, h:675, title:'נחל הפירים', tags: 'nahalpirim grapes winter'},
+{src:'images/map/big/nahalpirim66.jpg',w:1200, h:806, title:'נחל הפירים', tags: 'nahalpirim grapes autumn'},
+
+{src:'images/map/big/einelmeara5.jpg', w:1200, h:900, title:'עין אל מערה', tags: 'maayan einelmeara spring'},
+{src:'images/map/big/einelmeara1.jpg', w:1200, h:900, title:'עין אל מערה', tags: 'maayan einelmeara spring'},
+{src:'images/map/big/einelmeara3.jpg', w:675,  h:900, title:'עין אל מערה', tags: 'maayan einelmeara spring cave'},
+{src:'images/map/big/einelmeara8.jpg', w:1200, h:900, title:'עין אל מערה', tags: 'maayan einelmeara spring'},
+{src:'images/map/big/einelmeara9.jpg', w:1200, h:900, title:'עין אל מערה', tags: 'maayan einelmeara bloom spring'},
+
+{src:'images/map/big/amathabiar2.jpg', w:1200, h:800, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/amathabiar7.jpg', w:1200, h:801, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/amathabiar9.jpg', w:1200, h:800, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/amathabiar11.jpg', w:1200, h:800, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/einelmeara10.jpg', w:1200, h:900, title:'עין אל מערה', tags: 'bloom'},
+{src:'images/map/big/nahalpirim11.jpg',w:1200, h:675, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/nahalpirim18.jpg',w:1200, h:800, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/nahalpirim40.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/nahalpirim52.jpg',w:675, h:900, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/nahalpirim54.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/nahalpirim55.jpg',w:1200, h:900, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/nahalpirim59.jpg',w:1200, h:866, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/nahalpirim69.jpg',w:675, h:900, title:'נחל הפירים', tags: 'bloom'},
+{src:'images/map/big/nahalpirim76.jpg',w:1200, h:926, title:'נחל הפירים', tags: 'bloom'},
+];
+
+function openPhotoSwipe(subsetTag) {
+    
+	//console.log('Input tag = '+subsetTag);
+	
+// build subset of items according to TAG
+	var itemsSubset = [];
+	if (subsetTag == 'ALL') {
+		itemsSubset = items;
+	}
+	else {
+		for (i = 0; i < items.length; i++) {
+			//console.log('item['+i+'].tags='+items[i].tags);
+			x = items[i].tags.indexOf(subsetTag);
+			//console.log('x='+x);
+			if (x != -1) {
+				itemsSubset.push(items[i]);
+			}
+		}
+	}
+	
+	var pswpElement = document.querySelectorAll('.pswp')[0];
+    // define options (if needed)
+    var options = {
+      	history: false,
+      	//focus: false,
+
+        //showAnimationDuration: 0,
+        //hideAnimationDuration: 0,
+		
+		shareButtons: [
+			{id:'facebook', label:'Share on Facebook', url:'https://www.facebook.com/sharer/sharer.php?u={{url}}'}
+		]
+        
+    };
+
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, itemsSubset, options);
+    gallery.init();
+};
+
+//openPhotoSwipe(1);
+
+//document.getElementById('btn1').onclick = openPhotoSwipe(1);
+//document.getElementById('btn2').onclick = openPhotoSwipe(2);
