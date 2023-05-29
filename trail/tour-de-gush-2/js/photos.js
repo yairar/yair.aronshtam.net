8bab4b6bb693344ae7a7af4cce5f0b8b7c820f2f
yairar <yairar@users.noreply.github.com>
2023-05-29 22:43:05 +0300

    trail/tour-de-gush-2

---
+++ trail/tour-de-gush-2/js/photos.js
@@ -0,0 +1,115 @@
+var items = [
+{src:'images/map/big/mitzporhaelef1.jpg', w:1400, h:399, title:'מצפור האלף', tags: 'mitzpor1000'},
+{src:'images/map/big/mitzporhaelef2.jpg',  w:1400, h:589, title:'רואים את האוניות בנמל אשדוד ממצפור האלף', tags: 'mitzpor1000'},
+{src:'images/map/big/mitzporhaelef3.jpg',  w:1400, h:1050, title:'מצפור האלף', tags: 'mitzpor1000'},
+{src:'images/map/big/mitzporhaelef4.jpg',  w:1400,  h:1050,title:'מצפור האלף', tags: 'mitzpor1000'},
+{src:'images/map/big/mitzporhaelef5.jpg',w:1400, h:1050, title:'מצפור האלף', tags: 'mitzpor1000'},
+{src:'images/map/big/mitzporhaelef6.jpg', w:1400, h:933, title:'מצפור האלף', tags: 'mitzpor1000'},
+{src:'images/map/big/mitzporhaelef7.jpg', w:1400, h:1050, title:'מצפור האלף', tags: 'mitzpor1000'},
+{src:'images/map/big/mitzporhaelef8.jpg', w:1400, h:1050, title:'רואים את האוניות בנמל אשדוד ממצפור האלף', tags: 'mitzpor1000'},
+
+{src:'images/map/big/sdeboaz1.jpg', w:1400, h:1050, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz4.jpg', w:1400, h:1050, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz5.jpg', w:1400, h:1050, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz6.jpg', w:1400, h:1050, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz10.jpg', w:1400, h:1050, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz11.jpg', w:1400, h:1050, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz12.jpg', w:1400, h:933, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz13.jpg', w:1400, h:933, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz14.jpg', w:1400, h:1050, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz15.jpg', w:1400, h:1051, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/hurvasdeboaz2.jpg', w:2000, h:737, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/waditau1.jpg', w:1400, h:787, title:'ואדי טאו', tags: 'sdeboaz'},
+{src:'images/map/big/waditau2.jpg', w:1400, h:788, title:'ואדי טאו', tags: 'sdeboaz'},
+{src:'images/map/big/waditau6.jpg', w:1400, h:1050, title:'ואדי טאו', tags: 'sdeboaz'},
+{src:'images/map/big/waditau11.jpg', w:1400, h:1050, title:'ואדי טאו', tags: 'sdeboaz'},
+{src:'images/map/big/waditau12.jpg', w:1400, h:1050, title:'ואדי טאו', tags: 'sdeboaz'},
+{src:'images/map/big/waditau13.jpg', w:1400, h:1050, title:'ואדי טאו', tags: 'sdeboaz'},
+{src:'images/map/big/waditau14.jpg', w:1400, h:1050, title:'ואדי טאו', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz2.jpg', w:1600, h:439, title:'שדה בועז', tags: 'sdeboaz'},
+{src:'images/map/big/sdeboaz3.jpg', w:1600, h:628, title:'שדה בועז', tags: 'sdeboaz'},
+
+{src:'images/map/big/nebidaniel8.jpg', w:1400, h:1050, title:'נבי דניאל', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel15.jpg', w:1400, h:1050, title:'נבי דניאל', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel3.jpg', w:1200, h:800, title:'נבי דניאל', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel7.jpg', w:1400, h:1050, title:'בריכת אין בגרות', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel9.jpg', w:1400, h:1050, title:'נבי דניאל', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel10.jpg', w:1400, h:1050, title:'נבי דניאל', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel12.jpg', w:1400, h:1050, title:'נבי דניאל', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel14.jpg', w:1400, h:920, title:'נבי דניאל', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel16.jpg', w:1400, h:1050, title:'נבי דניאל', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel17.jpg', w:1400, h:1050, title:'נבי דניאל', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel18.jpg', w:1400, h:1050, title:'נבי דניאל', tags: 'nebidaniel'},
+{src:'images/map/big/nebidaniel19.jpg', w:1400, h:1049, title:'נבי דניאל', tags: 'nebidaniel'},
+
+
+{src:'images/map/big/mizrah1.jpg', w:1400, h:1050, title:'תצפית לירדן', tags: 'mizrah'},
+{src:'images/map/big/mizrah2.jpg', w:1400, h:1050, title:'תצפית לירדן', tags: 'mizrah'},
+];
+
+function openPhotoSwipe(subsetTag) {
+    
+	//console.log('Input tag = '+subsetTag);
+	
+// build subset of items according to TAG
+	var itemsSubset = [];
+	if (subsetTag == 'ALL') {
+		itemsSubset = items;
+	}
+	else {
+		for (i = 0; i < items.length; i++) {
+			//console.log('item['+i+'].tags='+items[i].tags);
+			x = items[i].tags.indexOf(subsetTag);
+			//console.log('x='+x);
+			if (x != -1) {
+				itemsSubset.push(items[i]);
+			}
+		}
+	}
+	
+	var pswpElement = document.querySelectorAll('.pswp')[0];
+    // define options (if needed)
+    var options = {
+      	history: false,
+      	//focus: false,
+
+        //showAnimationDuration: 0,
+        //hideAnimationDuration: 0,
+		
+		shareButtons: [
+			{id:'facebook', label:'Share on Facebook', url:'https://www.facebook.com/sharer/sharer.php?u={{url}}'}
+		]
+        
+    };
+
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, itemsSubset, options);
+    gallery.init();
+};
+
+function openPhotoSwipeVideo(id) {
+	
+	var itemsSubset =[
+	  {
+		html: '<video controls autoplay><source src="images/video/'+id+'.mp4" type="video/mp4"></video>'
+	  },
+	];
+	
+	var pswpElement = document.querySelectorAll('.pswp')[0];
+    // define options (if needed)
+    var options = {
+      	history: false,
+	
+		shareButtons: [
+			{id:'facebook', label:'Share on Facebook', url:'https://www.facebook.com/sharer/sharer.php?u={{url}}'}
+		]
+        
+    };
+
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, itemsSubset, options);
+    gallery.init();	
+}
+
+//openPhotoSwipe(1);
+
+//document.getElementById('btn1').onclick = openPhotoSwipe(1);
+//document.getElementById('btn2').onclick = openPhotoSwipe(2);
