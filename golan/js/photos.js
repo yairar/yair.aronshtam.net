b72ebde34ea89d16c8e393f719c0b17582f4f42e
yairar <yairar@users.noreply.github.com>
2024-10-14 23:20:56 +0300

    golan

--- golan/js/photos.js
+++ golan/js/photos.js
@@ -6,16 +6,24 @@ var photo_items = [
 
 {src:'images/map/big/einnov1.jpg', w:1600, h:1200, title:'עין נוב', tags: 'einnov maayan summer'},
 
+{src:'images/map/big/einoteden1.jpg', w:1600, h:1200, title:'עיינות עדן', tags: 'einoteden maayan autumn'},
+
 {src:'images/map/big/elal1.jpg', w:1600, h:1200, title:'נחל אל על', tags: 'elal autumn waterfall'},
 {src:'images/map/big/elal2.jpg', w:1600, h:1200, title:'נחל אל על', tags: 'elal autumn waterfall waterfall_lavan'},
 {src:'images/map/big/elal5.jpg', w:1600, h:1200, title:'נחל אל על', tags: 'elal autumn'},
 
+{src:'images/map/big/katzr1.jpg', w:1600, h:1198, title:'קצר ברדוויל', tags: 'katzr viewpoint winter'},
+{src:'images/map/big/katzr2.jpg', w:1600, h:1200, title:'קצר ברדוויל', tags: 'katzr viewpoint autumn'},
+
 {src:'images/map/big/mitzpefishgop1.jpg', w:1600, h:1200, title:'מצפה פישגופ, אפיק', tags: 'fishgop afik mitzpe'},
+
+{src:'images/map/big/meitzar1.jpg', w:1600, h:1200, title:'נחל מיצר', tags: 'meitzar spring viewpoint'},
 
 {src:'images/map/big/nov1.jpg', w:1600, h:1201, title:'נוב', tags: 'nov autumn'},
 {src:'images/map/big/nov5.jpg', w:1600, h:1200, title:'נוב', tags: 'nov spring sunset'},
 
-{src:'images/map/big/wadinov1.jpg', w:1600, h:1200, title:'', tags: 'wadinov winter'},
+{src:'images/map/big/wadinov1.jpg', w:1600, h:1200, title:'', tags: 'wadinov winter flow'},
+{src:'images/map/big/wadinov2.jpg', w:1600, h:1200, title:'', tags: 'wadinov winter'},
 
 
 ];
