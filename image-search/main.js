4a0fe2e8f146a0067db80363cd6ff9f9cf21dafe
yairar <yairar@users.noreply.github.com>
2024-10-15 14:35:55 +0300

    image-search: download button

--- image-search/main.js
+++ image-search/main.js
@@ -237,8 +237,11 @@ function displayImages(images, searchTerm) {
 			// DEEP LINKING
 			locationHash: false,
 
-			viewerToolbar: { display:true },
-			viewerTools: { bottom:'label'}
+			viewerTools: {
+				topRight: 'downloadButton, zoomButton, fullscreenButton, closeButton',
+				bottom:'label'
+			}
+
 		});
 	}
 }
