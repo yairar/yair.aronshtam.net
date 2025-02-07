a8d49104dbe4dca6faf4685f9a907556c6972745
yairar <yairar@users.noreply.github.com>
2025-02-07 14:18:37 +0200

    chrome_extension\extension_code v3

---
+++ chrome_extension/extension_code/options.js
@@ -0,0 +1,31 @@
+// Saves options to chrome.storage
+const saveOptions = () => {
+  const fvs = document.getElementById('fvs').value;
+  
+
+  chrome.storage.sync.set(
+    { fvs: fvs },
+    () => {
+      // Update status to let user know options were saved.
+      const status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(() => {
+        status.textContent = '';
+      }, 1000);
+    }
+  );
+};
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+const restoreOptions = () => {
+  chrome.storage.sync.get(
+    { fvs: 'show' },
+    (items) => {
+      document.getElementById('fvs').value = items.fvs;
+    }
+  );
+};
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
